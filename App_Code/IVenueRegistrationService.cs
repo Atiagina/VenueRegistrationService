ca9be59e48a7466198de755407dcccb8f46c6198
Anna <atiagina@gmail.com>
2016-02-08 11:55:51 -0800

    Adds Venue Registration Service files
    ADO entities, WCF service etc

---
+++ App_Code/IVenueRegistrationService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IVenueRegistrationService" in both code and config file together.
+[ServiceContract]
+public interface IVenueRegistrationService
+{
+    [OperationContract]
+    int VenueRegistration(VenueLite v);
+
+   [OperationContract]
+    int VenueLogin(string username, string password);
+}
+
+[DataContract]
+
+public class VenueLite
+{
+    [DataMember]
+    public string Name { set; get; }
+
+    [DataMember]
+    public string Address { set; get; }
+
+    [DataMember]
+    public string City { set; get;}
+
+    [DataMember]
+    public string State { set; get; }
+
+    [DataMember]
+    public string ZipCode { set; get; }
+
+    [DataMember]
+    public string Phone { set; get; }
+
+    [DataMember]
+    public string Email { set; get; }
+
+    [DataMember]
+    public string WebPage { set; get; }
+
+    [DataMember]
+    public int AgeRestriction { set; get; }
+
+    [DataMember]
+    public string UserName { set; get; }
+
+    [DataMember]
+    public string PasswordPlain { set; get; }
+}
---
+++ App_Code/VenueRegistrationService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "VenueRegistrationService" in code, svc and config file together.
+public class VenueRegistrationService : IVenueRegistrationService
+{
+
+    ShowTrackerEntities st = new ShowTrackerEntities();
+    public int VenueRegistration(VenueLite v){
+        Venue vt = new Venue();
+        int result = st.usp_RegisterVenue(v.Name, v.Address, v.City, v.State, v.ZipCode, v.Phone, v.Email, v.WebPage, v.AgeRestriction, v.UserName, v.PasswordPlain);
+        return result;
+    }
+
+   public  int VenueLogin(string username, string password) {
+
+       int result = st.usp_venueLogin(username, password);
+       if (result != -1)
+       {
+           var key = from k in st.VenueLogins
+                     where k.VenueLoginUserName.Equals(username)
+                     select new { k.VenueKey };
+           foreach (var k in key)
+           {
+               result = (int)k.VenueKey;
+           }
+       }
+
+       return result;
+    }
+}
